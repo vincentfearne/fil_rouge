f4c0a68480b6bdd00101906efe58fc5d12e0d638
vincentfearne <vincent.fearne@gmail.com>
2023-10-03 16:24:52 +0200

    03.10

--- src/Controller/CategorieController.php
+++ src/Controller/CategorieController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\CategorieRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -9,10 +10,10 @@ use Symfony\Component\Routing\Annotation\Route;
 class CategorieController extends AbstractController
 {
     #[Route('/categorie', name: 'app_categorie')]
-    public function index(): Response
+    public function index(CategorieRepository $repository): Response
     {
         return $this->render('categorie/index.html.twig', [
-            'controller_name' => 'CategorieController',
+            'categorie' => $repository->findAll()
         ]);
     }
 }
